f824e538304ad0297db00764f5d9313d4cb0ea8d
Jose Solano <joseandressolanomora@gmail.com>
2020-02-20 11:39:21 -0600

    Se crea la estructura inicial del proyecto

---
+++ cmake-build-debug/CMakeFiles/Grafo-Dijkstra.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Grafo-Dijkstra.dir/main.cpp.o"
+  "Grafo-Dijkstra.pdb"
+  "Grafo-Dijkstra"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Grafo-Dijkstra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by jose on 20/02/20.
+//
+
+//Llamada a librerías
+#include<stdio.h>
+#include<stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+//Función principal
+int main(void)
+{
+        cout<<"**GRAFOS DIJKSTRA**"<<endl;
+
+    return 0;
+}
